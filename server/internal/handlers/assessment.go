08c607754427df2bf91a806aea53167003b145c9
Jan Vargas <j.vargas.machaj@gmail.com>
2025-07-30 02:59:18 +0000

    Styling changes, streamlining appearance.

--- server/internal/handlers/assessment.go
+++ server/internal/handlers/assessment.go
@@ -8,6 +8,7 @@ import (
 	"crapp-go/internal/repository"
 	"crapp-go/views"
 
+	"github.com/a-h/templ"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -42,10 +43,10 @@ func (h *AssessmentHandler) Start(c *gin.Context, isHTMX bool) {
 	component := views.AssessmentPage(currentQuestion, state.CurrentQuestionIndex, len(state.QuestionOrder))
 
 	if isHTMX {
-		component.Render(c, c.Writer)
+		component.Render(c.Request.Context(), c.Writer)
 	} else {
 		// Render the full page for direct navigation
-		views.Page("Assessment", true).Render(c.Request.Context(), c.Writer)
+		views.Layout("Assessment", true).Render(templ.WithChildren(c.Request.Context(), component), c.Writer)
 	}
 }
 
